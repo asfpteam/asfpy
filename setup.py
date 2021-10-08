ff5e2a40802643aaae1811a332f04dafc77b3fc5
Ari Dyckovsky <aridyckovsky@gmail.com>
2021-10-08 13:41:00 -0400

    hotfix: udpate version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="asfpy-phd",
-    version="0.4.2",
+    version="0.4.3",
     author="Ari Dyckovsky",
     author_email="aridyckovsky@gmail.com",
     description="A collection of Python scripts for the Application Statement Feedback Program's logistics needs",
